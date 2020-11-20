4f0f0b46d76af6ee7247cbda0f03f22b9e83b1b2
Konrad Gorski <konradgrsk@gmail.com>
2020-11-20 15:44:41 +0100

    Updates

---
+++ precache-manifest.474ecd277c2963af1db24e66ff8fa7b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "212fb90c782c7b5f0a25f0b4a436c5e2",
+    "url": "/pixabay-app/index.html"
+  },
+  {
+    "revision": "83d937a07d6f10b29c36",
+    "url": "/pixabay-app/static/css/main.944a9eac.chunk.css"
+  },
+  {
+    "revision": "b08208635751c2b5ca6b",
+    "url": "/pixabay-app/static/js/2.a82f1941.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pixabay-app/static/js/2.a82f1941.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83d937a07d6f10b29c36",
+    "url": "/pixabay-app/static/js/main.ab5ae9a4.chunk.js"
+  },
+  {
+    "revision": "0c0a548ca0dd190c0cff",
+    "url": "/pixabay-app/static/js/runtime-main.3b0380d8.js"
+  }
+]);
